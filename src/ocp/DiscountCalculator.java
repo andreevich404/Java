fa77eb7e93d8917b7fbbf48028daa1b1914ab779
Ivan Andreevich <ivan_tolkachev.business@mail.ru>
2025-11-03 15:48:59 +0700

    feat: refactor code to follow SRP, OCP, LSP principles

---
+++ src/ocp/DiscountCalculator.java
@@ -0,0 +1,20 @@
+package ocp;
+
+import java.util.Map;
+
+public class DiscountCalculator {
+    private final Map<String, DiscountStrategy> strategies;
+
+    public DiscountCalculator() {
+        strategies = Map.of(
+                "regular", new RegularDiscount(),
+                "vip", new VipDiscount(),
+                "super_vip", new SuperVipDiscount()
+        );
+    }
+
+    public double calculateDiscount(String userType, double price) {
+        DiscountStrategy strategy = strategies.getOrDefault(userType, p -> 0.0);
+        return strategy.applyDiscount(price);
+    }
+}
