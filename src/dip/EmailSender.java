c6e0ac5038edf72a5d7d701c97e4a92d43cb1581
Ivan Andreevich <ivan_tolkachev.business@mail.ru>
2025-11-10 09:10:27 +0700

    refactor(dip): make EmailSender implement MessageSender interface

---
+++ src/dip/EmailSender.java
@@ -0,0 +1,9 @@
+package dip;
+
+public class EmailSender implements MessageSender {
+
+    @Override
+    public void send(String message) {
+        System.out.println("Отправка письма: " + message);
+    }
+}
