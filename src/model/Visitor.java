aee6ee8aabad383d3494eed69d24d5cfd8f1fdfe
Ivan Andreevich <ivan_tolkachev.business@mail.ru>
2025-11-17 09:51:04 +0700

    feat: add project structure and data models

---
+++ src/model/Visitor.java
@@ -0,0 +1,14 @@
+package model;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Visitor {
+    private String name;
+    private String surname;
+    private String phone;
+    private boolean subscribed;
+    private List<Book> favoriteBooks;
+}
