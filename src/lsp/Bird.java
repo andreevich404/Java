0f4310eb8f2f7b19753fd0ddaf5aa08dd6c41db9
Ivan Andreevich <ivan_tolkachev.business@mail.ru>
2025-11-17 08:15:19 +0700

    chore: remove files from previous lab

--- src/lsp/Bird.java
+++
@@ -1,9 +0,0 @@
-package lsp;
-
-public abstract class Bird {
-    public abstract void eat();
-
-    public void fly() {
-        throw new UnsupportedOperationException("Эта птица не умеет летать");
-    }
-}
